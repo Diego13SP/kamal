671783639f3e4dead0be0991a1015274c2ce26bb
Diego13SP <diegopedrette@yahoo.com.br>
2022-02-14 23:26:04 -0300

    "primero commit"

---
+++ src/main/java/dipedt/kamaleao/KamaleaoApplication.java
@@ -0,0 +1,13 @@
+package dipedt.kamaleao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamaleaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamaleaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dipedt/kamaleao/KamaleaoApplicationTests.java
@@ -0,0 +1,13 @@
+package dipedt.kamaleao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamaleaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
